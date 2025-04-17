fd81b460850eb221424ae37856ddb96cd4b3bdde
nemethkoppany <koppany344@gmail.com>
2025-04-17 16:36:29 +0200

    validacio hozzaadasa sima

--- main.js
+++ main.js
@@ -60,10 +60,17 @@ for(const fieldElement of elementsOfField){//fieldElement tömb bejárása
     label.htmlFor = fieldElement.fieldid; //label htmlFor beállítása
     field.appendChild(label); //A label fieldh-ez adása
 
+    field.appendChild(document.createElement("br"))//Sortörés hozzáadása a fieldhez
+
     const input = document.createElement('input'); //input létrehozás
     input.id = fieldElement.fieldid; //input id beállítása
-    field.appendChild(document.createElement('br')); //Sortörés fieldhez adása
     field.appendChild(input); //Az input field-hez adása
+    
+    field.appendChild(document.createElement("br"))//Sortörés hozzáadása a fieldhez
+
+    const error = document.createElement("span") //error span létrehozása
+    error.className = "error"; //Adunk neki egy class-t
+    field.appendChild(error); //Hozzárakjuk a fieldhez
 }
 
 const buttonRegular = document.createElement("button"); //Button létrehozása
@@ -75,10 +82,23 @@ formRegular.addEventListener("submit", (e)=>{//Eventlistener a submit eseményre
 
     const contentObject = {} //Objektum létrehozása
     const inputs = e.target.querySelectorAll("input") //Megkeressük az összes inputot
+    let isValid = true; //az isValid alapétrelmezett értéke true
     for(const input of inputs){ //Végigmegyünk az inputokon
+        const error = input.parentElement.querySelector(".error"); //Eltároljuk egy változóban az első error classú elemet
+        if(!error){//Ha nincs ilyen elem
+            console.error("nincs errorfield") //Írjon ki hibaüzenetet
+            return; //Lépjünk ki az if-ből
+        }
+        error.textContent ="";//Töröljük a hibaüzenetet
+        if(input.value === ""){//Ha nincs semmi a bementi mezőben
+            error.textContent = "Kötelező kitölteni" //Adjon hibát;
+            isValid = false; //És legyen az isValid értéke false;
+        }
+
         contentObject[input.id] = input.value; //A contentObject értékének az id-je legyen egynelő az input értékével
     }
-    tomb.push(contentObject); //És azt töltsük bele a táblázatba
+    if(isValid){//Ha az isValid értéke igaz maradt
+        tomb.push(contentObject); //És azt töltsük bele a táblázatba
     const tr = document.createElement("tr") //HTML elem létzrehozása
     tbody.appendChild(tr);//Hozzáadjuk az egyel fölötti réteghez
 
@@ -93,6 +113,7 @@ formRegular.addEventListener("submit", (e)=>{//Eventlistener a submit eseményre
     const szazadtd = document.createElement("td");//HTML elem létzrehozása
     szazadtd.textContent = contentObject.century; //Értékadás
     tr.appendChild(szazadtd);//Hozzáadjuk az egyel fölötti réteghez
+    }
 });
 
 divAsAContainer.appendChild(divTable); //A table container-hez adása
