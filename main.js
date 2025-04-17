05fdf2de58673306808355ebff5fe48aa9afa5fc
nemethkoppany <koppany344@gmail.com>
2025-04-17 14:30:30 +0200

    tablazat hozzadasa sima

--- main.js
+++ main.js
@@ -13,6 +13,25 @@ const divAsAContainer = divMaker('container'); //container létrehozás
 document.body.appendChild(divAsAContainer); //container a body-hoz adása
 const divTable = divMaker('table');  //table létrehozás
 
+
+const tableRegular = document.createElement('table'); //table létrehozás
+divTable.appendChild(tableRegular); //table a divhez adása
+
+const thead = document.createElement('thead'); //thead létrehozás
+tableRegular.appendChild(thead); //thead a tablehez adása
+
+const tr = document.createElement('tr'); //tr létrehozás
+thead.appendChild(tr); //tr a theadhez adása
+
+const headerContent = ["Név", "Számjegyek száma", "Század"]; //header tartalom
+for(const content of headerContent){ //headerContent tömb bejárása
+    const th = document.createElement('th'); //th létrehozás
+    th.innerHTML = content; //th tartalom beállítása
+    tr.appendChild(th); //th a trhez adása
+}
+const tbody = document.createElement('tbody'); //tbody létrehozás
+tableRegular.appendChild(tbody); //tbody a tablehez adása
+
 const divForm = divMaker('form'); //form létrehozás
 
 divAsAContainer.appendChild(divTable); //table a containerhez adása
