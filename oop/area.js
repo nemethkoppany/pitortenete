0a021a0cd09d99b03921dc6b8d5110ae4c218232
nemethkoppany <koppany344@gmail.com>
2025-04-17 14:34:50 +0200

    tablazat letrehozasa oop

--- oop/area.js
+++ oop/area.js
@@ -1,4 +1,14 @@
 class Area{
+
+    /**
+     * @type {HTMLDivElement}
+     */
+    #div //privát változó
+
+    get div(){ //getter, amivel elérjük a div-et
+        return this.#div; //visszaadja a div-et
+    }
+
     /**
      * 
      * @param {string} nameOfTheClass 
@@ -11,8 +21,31 @@ class Area{
             document.body.appendChild(divContainer); //container a body-hoz adása
         }
 
-        const div = document.createElement('div'); //div létrehozás
-        div.className = nameOfTheClass; //className beállítása
-        divContainer.appendChild(div); //a div containerhez adása
+        this.#div = document.createElement('div'); //div létrehozás
+        this.#div.className = nameOfTheClass; //className beállítása
+        divContainer.appendChild(this.#div); //div a containerhez adása
+    }
+}
+
+class Table extends Area{
+    constructor(nameOfTheClass){
+        super(nameOfTheClass); //szuper osztály konstruktora
+        const table = document.createElement('table'); //table létrehozás
+        this.div.appendChild(table); //table a divhez adása
+
+        const thead = document.createElement('thead'); //thead létrehozás
+        table.appendChild(thead); //thead a tablehez adása
+
+        const tr = document.createElement('tr'); //tr létrehozás
+        thead.appendChild(tr); //tr a theadhez adása
+
+        const headerContent = ["Név", "Számjegyek száma", "Század"]; //header tartalom
+        for(const content of headerContent){ //headerContent tömb bejárása
+            const th = document.createElement('th'); //th létrehozás
+            th.innerHTML = content; //th tartalom beállítása
+            tr.appendChild(th); //th a trhez adása
+        }
+        const tbody = document.createElement('tbody'); //tbody létrehozás
+        table.appendChild(tbody); //tbody a tablehez adása
     }
 }
